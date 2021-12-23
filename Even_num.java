fb7640616275e0b3342f05f7e32af50e848f0601
Bharathajxworkz <bharathaj2211.xwokz@gmail.com>
2021-12-23 20:39:15 +0530

    using if_else

---
+++ Even_num.java
@@ -0,0 +1,18 @@
+class Even_num{
+	public static void main (String[] args){
+
+
+	byte evenNum = 9;
+		if(evenNum %2==0){
+			System.out.println("its odd number");
+
+			}else{
+			System.out.println("its even number");
+			}
+
+
+
+
+
+}
+}
